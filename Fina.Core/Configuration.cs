cf09c5ccbdfcbb72c83c2db28f52550822128e61
fabinho1992 <f.santosdev1992@gmail.com>
2024-05-29 15:03:43 -0300

    RequestResponse

---
+++ Fina.Core/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using Fina.Core.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core
+{
+    public class Configuration 
+    {
+        public const int DefaultPageNumber = 1;// número da página
+        public const int DefaultPageSize = 25;//quantidade de registros na pagina
+    }
+}
