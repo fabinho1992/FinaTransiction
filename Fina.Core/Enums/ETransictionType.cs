878ba603357181baefe6000ba21b251d41a79339
fabinho1992 <f.santosdev1992@gmail.com>
2024-05-29 07:54:48 -0300

    Resolvendo conflitos

---
+++ Fina.Core/Enums/ETransictionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Enums
+{
+    public enum ETransictionType
+    {
+        Deposit = 1, // Deposito
+        WithDraw = 2 // Retirada
+    }
+}
---
+++ Fina.Core/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using Fina.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? PaidOrReceivedAt { get; set; }
+        public ETransictionType Type { get; set; } = ETransictionType.WithDraw;
+        public long CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
